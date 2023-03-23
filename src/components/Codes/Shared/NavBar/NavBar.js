3adfc03a4ce460405b41f6ec6b61f8e14fd13e20
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-03-23 19:45:19 +0600

    nav bar style

--- src/components/Codes/Shared/NavBar/NavBar.js
+++ src/components/Codes/Shared/NavBar/NavBar.js
@@ -44,7 +44,7 @@ const {user, logUser, logOut} = useContext(AuthContext)
 				<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
 			</label>
 			<ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-				<li><Link to='/'>Homepage</Link></li>
+				<li><Link to='/'>Home</Link></li>
 				<li><Link>Portfolio</Link></li>
 				<li><Link to='/about'>About</Link></li>
 			</ul>
@@ -53,7 +53,7 @@ const {user, logUser, logOut} = useContext(AuthContext)
 		   {/* For Desktop */}
 			<div className="navbar-start md:block hidden">
 			<ul className="menu menu-horizontal px-1">
-			<li><Link to='/'>Home page</Link></li>
+			<li><Link to='/'>Home</Link></li>
 			
 			{user&&
 			<li><Link to='/dashboard'>Dashboard</Link></li>
