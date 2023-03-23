3ceebc5a9f3ed2618f01d53b4dcc7838534d6d98
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-03-23 18:49:01 +0600

    categories wise dynamic data in dynamic route

--- src/components/Codes/Pages/ProductCategoryDetails/ProductCategoryDetails.js
+++ src/components/Codes/Pages/ProductCategoryDetails/ProductCategoryDetails.js
@@ -33,7 +33,7 @@ console.log('data', getData);
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-center">
             {
                 getData?.map((data) => 
-                <div className="card h-[600px]  mx-5 group bg-base-100 shadow-xl">
+                <div className="card h-[600px] border border-gray-300 mx-5 group bg-base-100 shadow-xl">
                 <img className='w-full h-80' src={data?.image} alt="img" />
                 <div className="card-body">
 
